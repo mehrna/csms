3654c2bf6313d0381f9f35b9b1adbf5936c28658
Mehrna <mehran.barzandeh@gmail.com>
2022-03-24 17:19:56 +0430

    Refactor action to use repositories

--- src/Application/Actions/Charge/RateChargeDetailRecordAction.php
+++ src/Application/Actions/Charge/RateChargeDetailRecordAction.php
@@ -6,20 +6,38 @@ namespace App\Application\Actions\Charge;
 
 use App\Application\Actions\Action;
 use App\Domain\Charge\ChargeDetailRecord;
+use App\Domain\Charge\ChargeRepository;
 use App\Domain\Charge\Rate;
 use App\Domain\Charge\RateCalculator;
+use App\Domain\Charge\RateRepository;
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Log\LoggerInterface;
 
 class RateChargeDetailRecordAction extends Action
 {
+    protected ChargeRepository $chargeRepository;
+    protected RateRepository $rateRepository;
+
+    public function __construct(LoggerInterface $logger, ChargeRepository $chargeRepository, RateRepository $rateRepository)
+    {
+        parent::__construct($logger);
+        $this->chargeRepository = $chargeRepository;
+        $this->rateRepository = $rateRepository;
+    }
+
     /**
      * @throws \JsonException
      */
     protected function action(): Response
     {
         $inputs = $this->getFormData();
+
         $rate = Rate::cast($inputs['rate'] ?? []);
+        $rateId = $this->rateRepository->store($rate);
+
         $cdr = ChargeDetailRecord::cast($inputs['cdr'] ?? []);
+        $this->chargeRepository->store($cdr, $rateId);
+
         $rateCalculator = new RateCalculator($rate, $cdr);
 
         return $this->respondWithData($rateCalculator);
